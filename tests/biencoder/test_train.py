99afe2e883ce6e8295751fab14fa017543360c8e
izuna385 <izuna385@gmail.com>
2021-05-09 15:24:56 +0900

    add test for biencoder training

--- tests/biencoder/test_train.py
+++ tests/biencoder/test_train.py
@@ -1,6 +1,8 @@
 from jel.biencoder.train import biencoder_training
 from allennlp.models import Model
+from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
+import pdb
 
-def train_test():
-    model = biencoder_training()
-    assert type(model) == Model
+def test_biencoder_training():
+    embedder, mention_encoder, entity_encoder = biencoder_training(debug=True)
