4b84359f8fb553db96bf9b2ad93c0c51f10ab280
izuna385 <izuna385@gmail.com>
2021-05-23 18:37:08 +0900

    add ner span returner

--- jel/utils/common.py
+++ jel/utils/common.py
@@ -1,7 +1,23 @@
 import json
+import spacy
+import logging
+from typing import Tuple, List,Dict
+
+nlp = spacy.load('ja_core_news_md')
 
 def jopen(file_path: str) -> dict:
     with open(file_path, 'r') as f:
         j = json.load(f)
 
     return j
+
+def return_ner_span(text: str) -> List[Dict]:
+    '''
+    :param text:
+    :return:
+    '''
+    doc = nlp(text=text)
+    ents = [{'text': ent.text,
+             'span': (ent.start_char, ent.end_char)} for ent in doc.ents]
+
+    return ents
---
+++ tests/utils/test_common.py
@@ -0,0 +1,8 @@
+from jel.utils.common import return_ner_span
+import pytest
+import pdb
+
+def test_tokenize_with_anchored_text():
+  text_sample = "夏目漱石と本郷三丁目"
+  spans = return_ner_span(text_sample)
+  assert spans == [{'text': '夏目漱石', 'span': (0, 4)}, {'text': '本郷三丁目', 'span': (5, 10)}]
