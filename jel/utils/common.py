bf00528ee0ebb3f7fcec8f10f8024d64840cfa8b
izuna385 <izuna385@gmail.com>
2021-05-23 21:53:35 +0900

    fix output of ner for output label itself

--- jel/utils/common.py
+++ jel/utils/common.py
@@ -2,8 +2,11 @@ import json
 import spacy
 import logging
 from typing import Tuple, List,Dict
+logger = logging.getLogger(__name__)
 
+logger.debug(msg='loading ja_core_news_md')
 nlp = spacy.load('ja_core_news_md')
+logger.debug(msg='loading ja_core_news_md finished.')
 
 def jopen(file_path: str) -> dict:
     with open(file_path, 'r') as f:
@@ -18,6 +21,7 @@ def return_ner_span(text: str) -> List[Dict]:
     '''
     doc = nlp(text=text)
     ents = [{'text': ent.text,
+             'label': ent.label_,
              'span': (ent.start_char, ent.end_char)} for ent in doc.ents]
 
     return ents
--- tests/utils/test_common.py
+++ tests/utils/test_common.py
@@ -5,4 +5,5 @@ import pdb
 def test_return_ner_span():
   text_sample = "夏目漱石と本郷三丁目"
   spans = return_ner_span(text_sample)
-  assert spans == [{'text': '夏目漱石', 'span': (0, 4)}, {'text': '本郷三丁目', 'span': (5, 10)}]
+  assert spans == [{'text': '夏目漱石', 'label': 'PERSON', 'span': (0, 4)},
+                   {'text': '本郷三丁目', 'label': 'FAC', 'span': (5, 10)}]
