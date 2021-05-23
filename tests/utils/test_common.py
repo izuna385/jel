815b372e8cc02d377e013c5b8b875da0bc85f4d1
izuna385 <izuna385@gmail.com>
2021-05-23 18:38:54 +0900

    fix ner test

--- tests/utils/test_common.py
+++ tests/utils/test_common.py
@@ -2,7 +2,7 @@ from jel.utils.common import return_ner_span
 import pytest
 import pdb
 
-def test_tokenize_with_anchored_text():
+def test_return_ner_span():
   text_sample = "夏目漱石と本郷三丁目"
   spans = return_ner_span(text_sample)
   assert spans == [{'text': '夏目漱石', 'span': (0, 4)}, {'text': '本郷三丁目', 'span': (5, 10)}]
